255337ee2a14258b4ec4ee424a5463a0f1d96d5c
GiulianoArk <calcagnigiuliano770@gmail.com>
2025-06-06 11:43:59 -0300

    formulario de contacto

--- src/components/MainContacto.jsx
+++ src/components/MainContacto.jsx
@@ -5,12 +5,61 @@ const MainContacto = () => {
     <div className='mainContacto'>
 
     <h2>Contacto</h2>
-      <form action="">
-        <input type="text" placeholder='Nombre' />
-        <input type="text" placeholder='Correo' />
-        <input type="text" placeholder='Título'/>
-        <input type="text" placeholder='Mensaje'/>
-      </form>
+      <div className='contenedorFormulario'>
+
+    <form className="form-container">
+  <div className="form-group">
+    <label htmlFor="nombre" className="form-label">Nombre</label>
+    <input
+      type="text"
+      className="form-input"
+      id="nombre"
+      name="nombre"
+      placeholder="Tu nombre"
+    />
+  </div>
+
+  <div className="form-group">
+    <label htmlFor="correo" className="form-label">Correo</label>
+    <input
+      type="email"
+      className="form-input"
+      id="correo"
+      name="correo"
+      placeholder="tucorreo@ejemplo.com"
+    />
+  </div>
+
+  <div className="form-group">
+    <label htmlFor="asunto" className="form-label">Asunto</label>
+    <input
+      type="text"
+      className="form-input"
+      id="asunto"
+      name="asunto"
+      placeholder="Motivo del mensaje"
+    />
+  </div>
+
+  <div className="form-group">
+    <label htmlFor="descripcion" className="form-label">Mensaje</label>
+    <textarea
+      className="form-textarea"
+      id="descripcion"
+      name="descripcion"
+      rows="4"
+      placeholder="Escribe tu mensaje aquí..."
+    ></textarea>
+  </div>
+
+  <div className="form-actions">
+    <button type="submit" className="form-button">Enviar</button>
+  </div>
+</form>
+
+
+
+      </div>
     </div>
   )
 }
